7766ad04041501291f73fbdbaf4c55689cf524f8
Loangkhan97 <solangisohailkhan903@gmail.com>
2025-10-15 08:54:49 +0500

    some animations added into js file

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Hero Section Loaded Successfully!");
 });
+
+
+// Scroll reveal for About section
+window.addEventListener("scroll", () => {
+  const about = document.querySelector(".about");
+  const aboutPos = about.getBoundingClientRect().top;
+  const screenPos = window.innerHeight / 1.3;
+  if (aboutPos < screenPos) {
+    about.classList.add("active");
+  }
+});
